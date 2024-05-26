8a6f06a78dc3ffd65e63e9d0f898257853a11592
RovshenAshirov <rovshenashirov1619@gmail.com>
2024-05-26 13:28:29 +0500

    practice with python

---
+++ 02-algorithm/python/01-adding-to-numbers.py
@@ -0,0 +1,7 @@
+def addNums(a, b):
+    summa = a + b
+    return summa
+
+a, b = map(int, input().split())
+
+print(addNums(a, b))
---
+++ 02-algorithm/python/03-calculation-of-N-factorial.py
@@ -0,0 +1,11 @@
+def faktorial(N):
+    i = 1
+    fakt = 1
+    while i != N + 1:
+        fakt = fakt * i        
+        i += 1
+    return fakt
+
+N = int(input())
+
+print(faktorial(N))
