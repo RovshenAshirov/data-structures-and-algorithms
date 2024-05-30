c47e40caa92bea2f5b0bad8d69858a05544f2bb0
RovshenAshirov <rovshenashirov1619@gmail.com>
2024-05-30 14:49:56 +0500

    recursion

---
+++ 07-recursion-and-stack/python/count.py
@@ -0,0 +1,14 @@
+# 10 dan 1 gacha teskari sanash dasturi
+
+def sana(n):
+    print(n)
+    sana(n-1)
+
+# ----------------
+
+def sana(n):
+    print(n)
+    if n <= 1:  # to'xtash sharti
+        return
+    else:  # rekursiya sharti
+        sana(n-1)
