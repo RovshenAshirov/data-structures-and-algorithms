9df67c208b28b534272d49617eef5a9a8d50e0df
RovshenAshirov <rovshenashirov1619@gmail.com>
2024-06-01 23:37:38 +0500

    graph

---
+++ 11-graph/python/graph.py
@@ -0,0 +1,15 @@
+# Graph yaratamiz
+
+if __name__ == '__main__':
+    graph = {
+        'siz': ['ali', 'vali', 'tohir'],
+        'ali': ['aziza', 'olim'],
+        'vali': ['botir', 'ziyoda'],
+        'tohir': ['elon musk', 'mohir'],
+        'aziza': [],
+        'olim': [],
+        'botir': [],
+        'ziyoda': ['aziza'],
+        'elon musk': [],
+        'mohir': []
+    }
